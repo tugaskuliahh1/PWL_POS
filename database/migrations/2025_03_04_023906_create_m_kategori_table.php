842463e440439471a715c9c3e8f7665737b19be6
tugaskuliahh1 <ismiatikaiik@gmail.com>
2025-03-04 22:24:35 +0700

    second commit

--- database/migrations/2025_03_04_023906_create_m_kategori_table.php
+++ database/migrations/2025_03_04_023906_create_m_kategori_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
+            $table->id('kategori_id');
+            $table->string('kategori_kode', 10)->uniqe();
+            $table->string('kategori_nama', 100);
             $table->timestamps();
         });
     }
