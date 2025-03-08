a7938edda4f1e6a7d8f82cfa0b028a0578c301ce
tugaskuliahh1 <ismiatikaiik@gmail.com>
2025-03-08 22:27:08 +0700

    second commit

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,18 +5,12 @@
     </head>
     <body>
         <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
+        <table border="1">
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
+                <th>Jumlah Pengguna</th>
             </tr>
             <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
+                <td>{{ $data }}</td>
             </tr>
         </table>
     </body>
