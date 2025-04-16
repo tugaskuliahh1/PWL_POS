6f224fbcaa398eee12209c5019f06e308db3c8df
tugaskuliahh1 <ismiatikaiik@gmail.com>
2025-04-16 21:10:19 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,9 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
@@ -18,4 +15,4 @@ Route::post('/user/tambah', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
+Route::get('/', [WelcomeController::class,'index']);
