5cf2b10a0b6b5d2d363f4f884e2f8c7dc14af295
=Ismi <=>
2025-05-23 14:54:43 +0700

    Js 8 praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -65,6 +65,7 @@ Route::middleware(['auth'])->group(function() {
         Route::get('/barang/import',[BarangController::class, 'import']);
         Route::post('/barang/import_ajax',[BarangController::class, 'import_ajax']);
         Route::get('/barang/export_excel',[BarangController::class, 'export_excel']); //export excel
+        Route::get('/barang/export_pdf',[BarangController::class, 'export_pdf']); //export pdf
 
         // Kategori Management
         Route::get('/kategori', [KategoriController::class, 'index']);
